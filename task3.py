e27c9ececdff0441e2e29b6ba71cd3bb2da57d89
MullapudiHarshitha <84438995+MullapudiHarshitha@users.noreply.github.com>
2024-04-02 19:21:00 +0530

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,10 @@
+import random
+lower= "abcdefghijklmnopqrstuvwxyz"
+upper= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+number= "0123456789"
+symbols= "!@#$%^&*()_+"
+all= lower + upper + number + symbols
+lenght=20
+temp= random.sample(all,lenght)
+password= "  " . join(temp)
+print(" MY PASSWORD IS :  " ,password)
